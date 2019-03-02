55b09f8294859edbf6eec8df4abb452d579620f0
Shashi Sharma <sharmashashi940@gmail.com>
2019-03-02 20:52:06 +0545

    decoded json

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class App extends StatefulWidget {
   @override
@@ -6,43 +9,75 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  StreamController title = new StreamController();
+  TextEditingController searchController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: appBar(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: appBar(),
+          body: Container(
+            child: Center(
+              child: StreamBuilder(
+                stream: title.stream,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData == false) {
+                    return CircularProgressIndicator();
+                  }
+                  return ListView(children: <Widget>[Text('${snapshot.data}')]);
+                },
+              ),
+            ),
+          ),
+        ));
   }
 
   Widget appBar() {
     return AppBar(
-      backgroundColor: Colors.blue,
-      actions: <Widget>[
-        Material(
-          borderRadius: BorderRadius.circular(32.0),
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(3.0),
-            child: Row(
-              children: <Widget>[
-                Flexible(
-                  child: TextField(),
-                ),
-                IconButton(
-                  icon: Icon(Icons.search, color: Colors.blue, size: 25.0),
-                  onPressed: () {
-                    //TODO
-                  },
-                ),
-              ],
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20.0, height: 1.5),
+              cursorColor: Colors.green,
+              controller: searchController,
+              decoration: InputDecoration.collapsed(
+                  hintText: 'e.g. London has fallen',
+                  hintStyle: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(32.0))),
             ),
           ),
-        )
-      ],
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: IconButton(
+                onPressed: () {
+                  getJson();
+                  //search.sink.add(searchController.text);
+                },
+                icon: Icon(Icons.search, size: 30.0, color: Colors.white)),
+          )
+        ],
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.green,
     );
   }
 
+  getJson() async {
+    var response = await http.get("https://yts.am/api/v2/list_movies.json");
+    var jsonBody = convert.jsonDecode(response.body);
+    var jsonData = jsonBody['data'];
 
+    title.sink.add(jsonData);
+  }
 }
